02efe7485a76dc014cbda286bf51a9f1ee97d309
Merve <mervetutoglu817@gmail.com>
2021-09-01 19:59:57 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	final int MARK = 80;
+        System.out.println (MARK >= 90 ?  "A" : (MARK >= 70 ? "B" : "C"));
+    }
+}
